121fb8a339df9a270538aa8141518b7dee5e1a43
Yuva <yuva@codemancers.com>
2013-07-24 12:36:12 +0530

    add vendor/assets images and fonts to precompile

--- lib/flatui/rails.rb
+++ lib/flatui/rails.rb
@@ -4,6 +4,12 @@ require "flatui/rails/version"
 module Flatui
   module Rails
     class Engine < ::Rails::Engine
+      initializer "flatui-rails.assets.precompile" do |app|
+        # include all the png files in vendor/assets/images folder
+        # include all the font files in vendor/assets/fonts folder
+        app.config.assets.precompile += %w(*.png)
+        app.config.assets.precompile += %w(*.eot *.svg *.tff *.woff)
+      end
     end
   end
 end
